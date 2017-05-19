d499f6900e8931ea7ae6afcfa0b28ab2c03cbad9
Evan Mullins <evanrm@gmail.com>
2017-05-19 08:19:42 -0400

    js updates for vuejs rebuild

--- js/scripts.js
+++ js/scripts.js
@@ -1,291 +1,58 @@
-/*
- * http://codepen.io/circlecube/pen/AXzxAv
- */
+var app = new Vue({
+		el: '#root',
+		data: {
+			max_errors_allowed: 5,
+			pause_game_allowed: false,
+			range_answer: [-6,6],
+			range_active: [0,9],
+			range_passive: [0,19],
+			range_total: [0,20],
+			init_timeout: 10000,
+			operation: '+', // '-', '/', '*'
+			errors: 0
+		},
 
-(function($){
+		methods: {
+			random () {
+			  return Math.round(Math.random()*this.range_answer[1]+this.range_answer[0]);
+			},
+			checkAnswer(answer){
+				if ( answer === this.random_term_top + this.random_term_bottom ) {
+					alert('correct!');
+					this.range_active = [0,9];
+					this.range_passive = [0,19];
+				} else {
+					this.errors++;
+					alert('nope!' + this.errors);
+				}
+			}
+		},
 
-/* SETTINGS VARIABLES */
+		computed: {
+			random_term_top(){
+				return Math.round(Math.random()*this.range_active[1]+this.range_active[0]);
+			},
 
-var elements = {
-	"app": $(".app"),
-  "output": $(".equation"),
-  "outputm": $(".message"),
-  "output1": $(".term1"),
-  "output2": $(".term2"),
-  "outputo": $(".operation"),
-	"input": $(".result"),
-	"timer": $(".timer-value"),
-};
+			random_term_bottom(){
+				return Math.round(Math.random()*this.range_passive[1]+this.range_passive[0]);
+			},
+			random_answers(){
+				var answers = [];
+				//get the correct answer
+				var correct = this.random_term_top + this.random_term_bottom
+				answers.push( correct );
+				//get three other answers - random differences from the range
+				answers.push( correct + this.random() );
+				answers.push( correct + this.random() );
+				answers.push( correct + this.random() );
+				//make sure answers are unique
+				//randomize the answers
+				answers.sort(function() { return 0.5 - Math.random() });
 
-var ui = {
-	"hearts": ".hearts",
-	"success": ".success",
-	"errors": ".errors",
-	"rounds": ".rounds",
-  "combo": ".combo",
-  "score": ".score",
-}
+				return answers;
 
-var output = {
-	errors: 0,
-	success: 0,
-	rounds: 0,
-  continued_success: 0,
-  combo: 1,
-  score: 0,
-};
-
-var config = {
-	max_errors_allowed: 5,
-	pause_game_allowed: false,
-  range_active: [0,9],
-  range_passive: [0,9],
-  range_total: [0,20],
-  init_timeout: 10000,
-}
-
-var cache = {
-	"result": null,
-	"timer": setTimeout(function(){}, 1),
-  "timeout": config.init_timeout,
-}
-
-
-
-
-/* UTILERY FUNCTIONS */
-
-var random = function (range)
-{
-  return Math.round(Math.random()*range[1]+range[0]);
-}
-
-var randomTerms = function(range)
-{
-  var result = random(range);
-  var top = random([0,result]);
-  var bottom = result-top;
-
-  var equation = {
-    'terms': [top,bottom],
-    'opperation': '+',
-    'result': result
-  };
-  return equation;
-}
-
-var startTimeout = function ()
-{
-  abortTimeout();
-  elements.timer.animate({width:'0%'}, cache.timeout, "linear");
-  cache.timer = setTimeout(function(){ validate(); }, cache.timeout)
-}
-
-var abortTimeout = function ()
-{
-  clearTimeout(cache.timer)
-  elements.timer.stop();
-  elements.timer.css({width: "100%"});
-}
+			}
+		}
 
 
-
-
-/* APPLICATION LOGIC FUNCTIONS */
-var generateTerms = function()
-{
-  var pasive = random(config.range_active);
-  var active = random(config.range_passive);
-  
-  cache.result = pasive + active;
-
-  if ( cache.reslut <= config.max_total ) {
-
-    var equation = {
-     'terms': [pasive,active],
-     'opperation': '+'
-    }
-    return equation;
-  }
-  else {
-    generateTerms();
-  }
-}
-var generateEq = function ()
-{
-  var eq = randomTerms(config.range_total);
-  cache.result = eq.result;
-
-  startTimeout();
-  
-  return eq;
-}
-
-var validate = function ()
-{
-  if (elements.input.val() == cache.result)
-    success();
-  else
-  	error();
-
-  setTimeout(function(){elements.input.removeClass("success error")},1000)
-  
-  run();
-}
-
-
-
-
-/* UI ACTIONS */
-
-var success = function ()
-{
-  elements.input.addClass("success");
-  output.success++;
-  output.continued_success++;
-  if (output.continued_success >= output.combo+1 && output.combo < 8) 
-  {
-    output.combo++;
-    output.continued_success = 1;
-    if (cache.timeout > 5000)
-      cache.timeout-=1000;
-  }
-  output.score+=1*output.combo;
-}
-
-var error = function ()
-{
-  elements.input.addClass("error");
-  output.errors++;
-  output.continued_success = 0;
-  output.combo = 1;
-  cache.timeout = config.init_timeout;
-  breakHeart();
-}
-
-var breakHeart = function ()
-{
-  var h = $(ui.hearts + " :not(.broken).fa-heart"); // Select non-broken hearts
-  $(h.get(h.length-1)).addClass("broken");
-}
-
-var refreshMark = function ()
-{
-	$.each(ui, function(key, element)
-	{
-		if (key == 'hearts') return;
-		$(element).html(output[key]);
-	})
-  elements.input.val("");
-}
-
-
-
-/* APPLICATION STATUS FUNCTIONS */
-
-var pause = function ()
-{
-  if ( config.pause_game_allowed ) {
-  	abortTimeout();
-    elements.outputm.html("Paused")
-    elements.app.addClass("paused")
-    cache.result = false;
-  }
-}
-
-var gameover = function ()
-{
-  abortTimeout();
-  elements.app.addClass("gameover");
-  elements.outputm.html("<i class='fa fa-frown-o'></i> GAME OVER");
-  // elements.input.attr("disabled",1);
-  elements.input.find(".message", "Click to play");
-  $(".mark").append('<div class="button restart">Restart</div>');
-  cache.result = Math.random();
-}
-
-var restart = function () 
-{
-  abortTimeout();
-  $('.button.restart').remove();
-  elements.app.removeClass("gameover");
-  elements.app.removeClass("paused")
-  elements.outputm.html("");
-  // elements.input.attr("disabled",0);
-  // elements.input.find(".message", "Click to play");
-  cache.result = false;
-  output.errors = 0;
-  output.success = 0;
-  output.rounds = 0;
-  output.continued_success = 0;
-  output.combo = 1;
-  output.score = 0;
-  generateHearts();
-  elements.input.trigger('focus');
-  setTimeout(run, 100);
-}
-
-
-/* ONE-TIME-RUN FUNCTIONS */
-
-var addListeners = function ()
-{
-	elements.input.on('focusin', function(){
-    if (!cache.result) setTimeout(run, 100);
-  });
-  
-  elements.input.on('focusout', function(){
-    if (output.errors < config.max_errors_allowed)
-      pause();
-  });
-  
-  elements.input.on('keyup', function(e){
-    if (e.which == 13 || ($(this).val()==cache.result)) validate();
-  });
-
-  $('.app').on('click', '.restart.button', function(e) {
-    restart();
-  });
-
-  $()
-  
-}
-
-var generateHearts = function ()
-{
-	$(ui.hearts).html("");
-	for (var i = 1; i <= config.max_errors_allowed; i++) {
-		$(ui.hearts).append("<i class='fa fa-heart'></i>");
-	};
-}
-
-
-
-
-/* MAIN */
-
-var run = function ()
-{
-  elements.app.removeClass("paused")
-  
-	output.rounds++;
-  refreshMark();
-  
-  if (output.errors >= config.max_errors_allowed)
-  	gameover()
-  else if (!$(":focus").hasClass("result") && config.pause_game_allowed && output.rounds > 1)
-		pause()
-	else {
-    var equation = generateEq();
-    elements.outputm.html('');
-    elements.output1.html(equation.terms[0]);
-    elements.output2.html(equation.terms[1]);
-    elements.outputo.html(equation.opperation);
-  }
-}
-
-/* RUN */
-generateHearts();
-addListeners();
-  
-})(jQuery);
+	});
