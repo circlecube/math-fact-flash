951c7ff571081ff5b0d9398f4a7c47378e7fd50a
Evan Mullins <emullins@brownbagmarketing.com>
2017-05-24 10:41:24 -0400

    add options in modal. add support for subtraction and multiplication and connect options in modal to the acutal values. update so values are just vars and no longer compute values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
+//register modal component
+Vue.component('modal', {
+	template: '#modal-template'
+});
+
+//app
 var app = new Vue({
 		el: '#app',
 		data: {
 			max_errors_allowed: 5,
 			pause_game_allowed: false,
 			range_answer: 6,
-			range_active: [0,9],
-			range_passive: [0,19],
+			active_val: '',
+			passive_val: '',
+			active_min: 0,
+			active_max: 9,
+			passive_min: 0,
+			passive_max: 19,
 			range_total: [0,20],
 			init_timeout: 10000,
 			operation: '+', // '-', '/', '*'
 			errors: 0,
 			valids: 0,
-			mode: 'game', // 'test'
+			answers: 0,
+			mode: 'game',	// 'game' - answer until correct. 
+							//	 'test' - only one answer per question
+			timer: false,	// to add a timer option
 			allow_negative: false,
 			fractions: false,
 			decimals: false,
-			state: 'on'//valid, error, on, end
+			state: 'on',	// valid, error, on, end
+			showModal: false,
 		},
 
 		methods: {
@@ -23,66 +37,90 @@ var app = new Vue({
 				// return Math.round(Math.random()*this.range_answer[1]+this.range_answer[0]);
 
 				// correct answer will always be the first index of answers
-				// distance meaning be the range +/- that the answer can be from the center
+				// distance meaning be the range +/- that the answer can be from the center (correct answer)
 				// get random number of distance
-				var num = Math.round(Math.random()*distance*2)-distance;
+				var num = Math.round( Math.random() * distance * 2 ) - distance;
 				// center number is the origin
+				// offset for correct
 				num += answers[0].value;
-				// must make sure the answer checks if `allow_negative`
+				// make sure the answer checks if `allow_negative`
 				if ( !this.allow_negative && num < 0 ) {
-					num *= -1;
+					// if negative get another
+					return this.random(answers, distance);
 				}
-				// must make sure the answer is unique
-				for (var i = 0; i<answers.length; i++){
+				// make sure the answer is unique
+				for (var i = 0; i < answers.length; i++){
 					if ( answers[i].value === num ) {
-						// console.log('found duplicate answer:',num,i);
-						//if duplicate is found recursively call again
-				  		return this.random(answers, distance)
+						// if duplicate is found recursively call again
+				  		return this.random(answers, distance);
 					}
 				}
 
 				return num;
 			},
+			getAnswer(){
+				switch(this.operation){
+					case '+':
+						return this.active_val + this.passive_val;
+					case '-':
+						return this.active_val - this.passive_val;
+					case '×':
+						return this.active_val * this.passive_val;
+					case '÷':
+						return (this.active_val / this.passive_val).toFixed(2);
+					default:
+						return this.active_val + this.passive_val;
+				}
+			},
 			checkAnswer(i){
-				if ( this.random_answers[i].value === this.random_term_top + this.random_term_bottom ) {
+				// get answer
+				var correct = this.getAnswer();
+
+				if ( this.random_answers[i].value === correct ) {
 					// alert/('correct!');
+					this.valids++;
 					this.state = 'valid';
 					this.random_answers[i].state = 'valid';
 					setTimeout(this.newCard, 500);
 				} else {
+					this.errors++;
 					this.state = 'error';
 					this.random_answers[i].state = 'error';
 					this.badAnswer();
 					// alert('nope! ' + this.errors + ' incorrect.');
 				}
+				this.answers++;
 			},
 			newCard(){
-				this.valids++;
-				//reset the source computed values and it will trigger vue to recalculate them.
-				this.range_active = [0,9];
-				this.range_passive = [0,19];
+				//reset the values
+				this.getRandomValues();
 				this.state = 'on';
 			},
 			badAnswer(){
-				this.errors++;
 				if ( this.errors > this.max_errors_allowed ) {
 					// this.state = 'end';
 				}
+			},
+			getRandomValues(){
+				this.active_val = Math.round( Math.random() * ( this.active_max - this.active_min ) ) + this.active_min;
+				this.passive_val = Math.round( Math.random() * ( this.passive_max - this.passive_min ) ) + this.passive_min;
+			},
+			reset(){
+				this.showModal = false;
+				this.errors = 0;
+				this.valids = 0;
+				this.answers = 0;
+				this.getRandomValues();
 			}
 		},
 
 		computed: {
-			random_term_top(){
-				return Math.round(Math.random()*this.range_active[1]+this.range_active[0]);
-			},
-
-			random_term_bottom(){
-				return Math.round(Math.random()*this.range_passive[1]+this.range_passive[0]);
-			},
+			
 			random_answers(){
 				var answers = [];
 				//get the correct answer
-				var correct = this.random_term_top + this.random_term_bottom
+				var correct = this.getAnswer();
+
 				answers.push( { 
 					value: correct,
 					state: 'on' //valid, correct 
@@ -101,7 +139,17 @@ var app = new Vue({
 
 				return answers;
 
+			},
+
+			grade(){
+				var average = Math.round(this.valids / this.answers * 100);
+				if ( isNaN(average) ) average = '-';
+				return average;
 			}
+		},
+
+		mounted(){
+			this.reset();
 		}
 
 
