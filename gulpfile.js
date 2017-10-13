943d53c289a5e3755e20767e12a7cfe68d339c1c
Evan Mullins <emullins@brownbagmarketing.com>
2017-10-13 15:07:59 -0400

    update gulpfile comments

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,9 @@
  *      2. CSS: Sass to CSS conversion, error catching, Autoprefixing, Sourcemaps,
  *         CSS minification, and Merge Media Queries.
  *      3. JS: Concatenates & uglifies Vendor and Custom JS files.
- *      4. Images: Minifies PNG, JPEG, GIF and SVG images.
+ *      //4. Images: Minifies PNG, JPEG, GIF and SVG images.
  *      5. Watches files for changes in CSS or JS.
- *      6. Watches files for changes in PHP.
+ *      //6. Watches files for changes in PHP.
  *      7. Corrects the line endings.
  *      8. InjectCSS instead of browser page reload.
  *      //9. Generates .pot file for i18n and l10n.
