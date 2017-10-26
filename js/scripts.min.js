e6bb3d3583ea3c4dda80f2b1e51c42ad3250486d
Evan Mullins <emullins@brownbagmarketing.com>
2017-10-26 14:31:04 -0400

    alphabetize methods for quick finding and update log for single ranges.

--- js/scripts.min.js
+++ js/scripts.min.js
@@ -52,7 +52,7 @@ var app = new Vue({
 			logs: localStorage.getItem('logs') ? JSON.parse(localStorage.getItem('logs')) : [],
 			displaylogs: false,
 			infomode: false,
-			autopilot: true,
+			autopilot: false,
 			autopilot_timeout: null,
 			autopilot_delay: 400,
 			card_timeout: null,
@@ -60,71 +60,7 @@ var app = new Vue({
 		},
 
 		methods: {
-			random(answers, distance) {
-				// return Math.round(Math.random()*this.range_answer[1]+this.range_answer[0]);
 
-				// correct answer will always be the first index of answers
-				// distance meaning be the range +/- that the answer can be from the center (correct answer)
-				// get random number of distance
-				var num = Math.round( Math.random() * distance * 2 ) - distance;
-				// center number is the origin
-				// offset for correct
-				num += answers[0].value;
-				// make sure the answer checks if `allow_negative`
-				if ( !this.allow_negative && num < 0 ) {
-					// if negative get another
-					//return this.random(answers, distance);
-					num *= -1; //make it positive
-				}
-				// make sure the answer is unique
-				for (var i = 0; i < answers.length; i++){
-					if ( answers[i].value === num ) {
-						// if duplicate is found recursively call again
-						return this.random(answers, distance);
-					}
-				}
-
-				return num;
-			},
-			randomNumber(x){
-				if (typeof x === 'undefined') { x = 10; }
-				return Math.floor(Math.random() * x) + 1;
-			},
-			flipcard(){
-				// console.log('flipcard');
-				if (this.cardstate === 'front') { 
-					this.cardstate = 'back';
-				} else {
-					this.cardstate = 'front';
-					this.displaylogs = false;
-				}
-
-				if (this.state === 'off'){
-					this.reset();
-				}
-			},
-			progresswidthcss(total){
-				if ( this.mode === 'practice' ) {
-					return 'width:' + (100 / this.current_card) + '%;';
-				} else {
-					return 'width:' + (100 / this.total_cards) + '%;';
-				}
-			},
-			getAnswer(){
-				//based on operation, calculate the correct answer
-				switch(this.operation){
-					case '+':
-						return this.active_val + this.passive_val;
-					case '-':
-						return this.active_val - this.passive_val;
-					case '×':
-						return this.active_val * this.passive_val;
-					case '÷':
-						return this.active_val / this.passive_val;
-					default:
-						return this.active_val + this.passive_val;
-				}
-			},
 			answerforme(){
 				var correct = this.getAnswer();
 
@@ -136,6 +72,13 @@ var app = new Vue({
 				}
 
 			},
+
+			badAnswer(){
+				if ( this.errors > this.max_errors_allowed ) {
+					// this.state = 'end';
+				}
+			},
+
 			checkAnswer(i){
 				// get correct answer
 				var correct = this.getAnswer();
@@ -191,54 +134,49 @@ var app = new Vue({
 					if ( this.current_card > 0 && this.autopilot ) {
 						this.autopilot_timeout = setTimeout(this.answerforme, this.card_delay + this.autopilot_delay);
 					}
+				}	 
+			},
 
-				}
-				
-				 
+			clearlog(){
+				this.logs = [];
+				// localStorage.setItem('logs', []);
 			},
-			newCard(){
-				//if practice mode
-				if ( this.mode === 'practice' ) {
-					//reset the values
-					this.getRandomValues();
-					this.state = 'on';
-				} else { // test mode
-				// if cards are out
-				if ( this.current_card <= 0 ) {
-						this.getReport();
-						this.state = 'off';
-					} else {
-						//reset the values
-						this.getRandomValues();
-						this.state = 'on';
-					}
-				}
-				this.answer_color = this.randomNumber();
-			},
-			badAnswer(){
-				if ( this.errors > this.max_errors_allowed ) {
-					// this.state = 'end';
-				}
-			},
-			getReport(){
-				this.endtime = new Date();
-				this.duration = moment(this.endtime).diff(moment(this.starttime), 'seconds');
-				var log = {
-					grade: this.grade,
-					total: this.total_cards,
-					operationlabel: this.operationlabel[this.operation],
-					operation: this.operation,
-					terms: this.actives + ' & ' + this.passives,
-					duration: this.duration,
-					time: new Date(),
-				};
-				// alert(log.operation +'('+ log.total +') '+ log.grade +'% '+ log.duration + 's');
-				this.logs.unshift(log);
-				// localStorage.setItem('logs', this.logs);
 
-				this.flipcard();
-				this.displaylog(true);
+			displaylog(show){
+				if (typeof show === 'undefined') { show = !this.displaylogs; }
+				this.displaylogs = show;
 			},
+
+			flipcard(){
+				// console.log('flipcard');
+				if (this.cardstate === 'front') { 
+					this.cardstate = 'back';
+				} else {
+					this.cardstate = 'front';
+					this.displaylogs = false;
+				}
+
+				if (this.state === 'off'){
+					this.reset();
+				}
+			},
+
+			getAnswer(){
+				//based on operation, calculate the correct answer
+				switch(this.operation){
+					case '+':
+						return this.active_val + this.passive_val;
+					case '-':
+						return this.active_val - this.passive_val;
+					case '×':
+						return this.active_val * this.passive_val;
+					case '÷':
+						return this.active_val / this.passive_val;
+					default:
+						return this.active_val + this.passive_val;
+				}
+			},
+
 			getRandomValues(){
 				if ( this.operation != '÷') { //addition subtraction and multiplication
 					this.active_val = Math.round( Math.random() * ( this.active_max - this.active_min ) ) + this.active_min;
@@ -261,6 +199,98 @@ var app = new Vue({
 					this.active_val = this.active_val * this.passive_val;
 				}
 			},
+
+			getReport(){
+				this.endtime = new Date();
+				this.duration = moment(this.endtime).diff(moment(this.starttime), 'seconds');
+
+				var terms_active = this.actives;
+				var terms_passive = this.passives;
+				if ( this.active_max === this.active_min ) {
+					terms_active = this.active_max;
+				}
+				if ( this.passive_max === this.passive_min ) {
+					terms_passive = this.passive_max;
+				}
+
+				var log = {
+					grade: this.grade,
+					correct: this.valids,
+					total: this.total_cards,
+					operationlabel: this.operationlabel[this.operation],
+					operation: this.operation,
+					terms: terms_active + ' & ' + terms_passive,
+					duration: this.duration,
+					time: new Date(),
+				};
+				// alert(log.operation +'('+ log.total +') '+ log.grade +'% '+ log.duration + 's');
+				this.logs.unshift(log);
+				// localStorage.setItem('logs', this.logs);
+
+				this.flipcard();
+				this.displaylog(true);
+			},
+
+			newCard(){
+				//if practice mode
+				if ( this.mode === 'practice' ) {
+					//reset the values
+					this.getRandomValues();
+					this.state = 'on';
+				} else { // test mode
+				// if cards are out
+				if ( this.current_card <= 0 ) {
+						this.getReport();
+						this.state = 'off';
+					} else {
+						//reset the values
+						this.getRandomValues();
+						this.state = 'on';
+					}
+				}
+				this.answer_color = this.randomNumber(8);
+			},
+
+			progresswidthcss(total){
+				if ( this.mode === 'practice' ) {
+					return 'width:' + (100 / this.current_card) + '%;';
+				} else {
+					return 'width:' + (100 / this.total_cards) + '%;';
+				}
+			},
+
+			random(answers, distance) {
+				// return Math.round(Math.random()*this.range_answer[1]+this.range_answer[0]);
+
+				// correct answer will always be the first index of answers
+				// distance meaning be the range +/- that the answer can be from the center (correct answer)
+				// get random number of distance
+				var num = Math.round( Math.random() * distance * 2 ) - distance;
+				// center number is the origin
+				// offset for correct
+				num += answers[0].value;
+				// make sure the answer checks if `allow_negative`
+				if ( !this.allow_negative && num < 0 ) {
+					// if negative get another
+					//return this.random(answers, distance);
+					num *= -1; //make it positive
+				}
+				// make sure the answer is unique
+				for (var i = 0; i < answers.length; i++){
+					if ( answers[i].value === num ) {
+						// if duplicate is found recursively call again
+						return this.random(answers, distance);
+					}
+				}
+
+				return num;
+			},
+
+			randomNumber(x){
+				if (typeof x === 'undefined') { x = 10; }
+				return Math.floor(Math.random() * x) + 1;
+			},
+			
 			reset(){
 				// console.log('start game:', this.mode);
 				this.state = 'on';
@@ -268,6 +298,7 @@ var app = new Vue({
 				this.errors = 0;
 				this.valids = 0;
 				this.answers = 0;
+				this.answer_color = this.randomNumber(8);
 				if ( this.mode === 'practice' ) {
 					this.current_card = 0;
 				} else {
@@ -278,9 +309,7 @@ var app = new Vue({
 
 				this.getRandomValues();
 			},
-			toggleinfomode(){
-				this.infomode = !this.infomode;
-			},
+
 			setDefaultRanges(){
 
 				//set ranges based on operation
@@ -307,6 +336,7 @@ var app = new Vue({
 				}
 				this.setRanges();
 			},
+
 			setRanges(){
 				//set ranges based on values of the select lists for active and passive numbers
 				//active split
@@ -317,15 +347,10 @@ var app = new Vue({
 				vals = this.passives.split('-');
 				this.passive_min = parseInt( vals[0] );
 				this.passive_max = parseInt( vals[1] );
+			},
 
-			},
-			displaylog(show){
-				if (typeof show === 'undefined') { show = !this.displaylogs; }
-				this.displaylogs = show;
-			},
-			clearlog(){
-				this.logs = [];
-				// localStorage.setItem('logs', []);
+			toggleinfomode(){
+				this.infomode = !this.infomode;
 			},
 			
 		},
@@ -344,6 +369,12 @@ var app = new Vue({
 
 		computed: {
 			
+			grade(){
+				var average = Math.round(this.valids / this.answers * 100);
+				if ( isNaN(average) ) average = '0';
+				return average;
+			},
+
 			random_answers(){
 				var answers = [];
 				//get the correct answer
@@ -361,25 +392,16 @@ var app = new Vue({
 					} );	
 				}
 				// console.table(answers);
-				
 				// make sure answers are unique
 				// and randomize the answers
 				answers.sort(function() { return 0.5 - Math.random() });
-
 				return answers;
-
 			},
 
-			grade(){
-				var average = Math.round(this.valids / this.answers * 100);
-				if ( isNaN(average) ) average = '0';
-				return average;
-			}
 		},
 
 		mounted(){
 			this.reset();
 		}
-
 
 	});
